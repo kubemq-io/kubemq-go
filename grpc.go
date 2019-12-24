8f62e6b21ca53cf631b0b3aef7f00787b3cc5fb1
kubemq <lior.nabat@kubemq.io>
2019-12-24 16:01:19 +0200

    - change protobuf to kubemq protobuf repo

--- grpc.go
+++ grpc.go
@@ -7,7 +7,7 @@ import (
 	"time"
 
 	"github.com/google/uuid"
-	"github.com/kubemq-io/kubemq-go/pb"
+	pb "github.com/kubemq-io/protobuf/go"
 	"go.opencensus.io/trace"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
