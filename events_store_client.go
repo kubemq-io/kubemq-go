200b92f0acb486060135e47f211f5888af91bd33
KubeMQ <lior.nabat@kubemq.io>
2021-04-05 16:46:12 +0300

    add client wrappers

--- events_store_client.go
+++ events_store_client.go
@@ -1,6 +1,9 @@
 package kubemq
 
-import "context"
+import (
+	"context"
+	"fmt"
+)
 
 type EventsStoreClient struct {
 	client *Client
@@ -20,10 +23,63 @@ func (es *EventsStoreClient) Send(ctx context.Context, message *EventStore) (*Ev
 	return es.client.SetEventStore(message).Send(ctx)
 }
 
-func (es *EventsStoreClient) Stream(ctx context.Context, eventsCh chan *EventStore, eventsResultCh chan *EventStoreResult, errCh chan error) {
-	es.client.StreamEventsStore(ctx, eventsCh, eventsResultCh, errCh)
+func (es *EventsStoreClient) Stream(ctx context.Context, onResult func(result *EventStoreResult, err error)) (func(msg *EventStore) error, error) {
+	if onResult == nil {
+		return nil, fmt.Errorf("events stream result callback function is required")
+	}
+	errCh := make(chan error, 1)
+	eventsCh := make(chan *EventStore, 1)
+
+	sendFunc := func(msg *EventStore) error {
+		select {
+		case eventsCh <- msg:
+			return nil
+
+		case <-ctx.Done():
+			return fmt.Errorf("context canceled during events message sending")
+		}
+	}
+	go func() {
+		eventsResultCh := make(chan *EventStoreResult, 1)
+		es.client.StreamEventsStore(ctx, eventsCh, eventsResultCh, errCh)
+		for {
+			select {
+			case result := <-eventsResultCh:
+				onResult(result, nil)
+			case err := <-errCh:
+				onResult(nil, err)
+			case <-ctx.Done():
+				return
+			}
+		}
+	}()
+	return sendFunc, nil
 }
 
-func (es *EventsStoreClient) Subscribe(ctx context.Context, channel, group string, errCh chan error, opt SubscriptionOption) (<-chan *EventStoreReceive, error) {
-	return es.client.SubscribeToEventsStore(ctx, channel, group, errCh, opt)
+func (es *EventsStoreClient) Subscribe(ctx context.Context, channel, group string, onEvent func(msg *EventStoreReceive, err error), opt SubscriptionOption) error {
+	if onEvent == nil {
+		return fmt.Errorf("events store subscription callback function is required")
+	}
+	errCh := make(chan error, 1)
+	eventsCh, err := es.client.SubscribeToEventsStore(ctx, channel, group, errCh, opt)
+	if err != nil {
+		return err
+	}
+	go func() {
+		for {
+			select {
+			case msg := <-eventsCh:
+				onEvent(msg, nil)
+			case err := <-errCh:
+				onEvent(nil, err)
+			case <-ctx.Done():
+				return
+			}
+		}
+	}()
+	return nil
+}
+
+func (es *EventsStoreClient) Close() error {
+	return es.client.Close()
 }
